c72ecae48327471b85d53e49a6d391220a6c8d45
yanghui <672859954@qq.com>
2018-09-01 15:28:46 +0800

    脚本

--- packege.sh
+++ packege.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 git pull
 mvn clean install -Dmaven.test.skip=true -Ppro
+echo "=================================mavne编译完成============================="
 docker build -t antelope .
+echo "=================================镜像打包完成==============================="
 docker stop antelope
-docker run -d -p  80:80 --rm --net my-net --name antelope --net my-net antelope
+echo "=================================停止容器antelope=========================="
+docker run -d -p 80:80 --rm --net my-net --name antelope antelope
+echo "=================================容器antelope启动完成=========================="
