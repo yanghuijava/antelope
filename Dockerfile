3b9862588b4382b67224484eb6ef6edbd2e55542
yanghui <672859954@qq.com>
2018-09-02 10:45:57 +0800

    脚本

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM java:8
 RUN mkdir -p /export/app/antelope/
 WORKDIR /export/app/antelope/
 ADD target/antelope.jar /export/app/antelope/
+EXPOSE 80
 ENTRYPOINT ["java","-jar","/export/app/antelope/antelope.jar","> /dev/null &"]
--- packege.sh
+++ packege.sh
@@ -6,6 +6,5 @@ docker build -t antelope .
 echo "=================================镜像打包完成==============================="
 docker stop antelope
 echo "=================================停止容器antelope=========================="
-EXPOSE 80
 docker run -d -p 80:80 --rm --net my-net --name antelope antelope
 echo "=================================容器antelope启动完成=========================="
